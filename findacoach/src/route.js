afcc7e6f485595fd1f9b928d72d66f4856d70d09
Siddhartha Gunturu <gunturu.siddhartha@gmail.com>
2023-07-04 21:46:53 -0400

    Added components and setup routing

--- findacoach/src/route.js
+++ findacoach/src/route.js
@@ -1,15 +1,22 @@
-import { createRoute, createWebHistory } from 'vue-route'
+import { createRouter, createWebHistory } from 'vue-route'
+
+import CoachDetail from './components/coaches/CoachDetail.vue';
+import CoachesList from './components/coaches/CoachesList.vue';
+import CoachRegistation from './components/coaches/CoachRegistration.vue';
+import ContactCoach from './components/requests/ContactCoach.vue';
+import RequestsReceived from './components/requests/RequestsReceived.vue';
+import NotFound from './components/NotFound.vue';
 
 const router =  createRouter({
     history: createWebHistory(),
     routes: [
         {path:'/', redirect: '/coaches' },
-        { path: '/coaches', component: null   },
-        { path: '/coaches/:id', component: null, children:[
-            { path: 'contact', component: null  } //coaches/id/contact
+        { path: '/coaches', component: CoachesList },
+        { path: '/coaches/:id', component: CoachDetail, children:[
+            { path: 'contact', component: ContactCoach  } //coaches/id/contact
         ]},
-        { path: '/register', component: null  },
-        { path: '/requests', component: null  },
-        { path: '/:notFound(.*)', component: null }, // adding reg expression(.*) to allow the route to accept any input passed
+        { path: '/register', component: CoachRegistation  },
+        { path: '/requests', component: RequestsReceived  },
+        { path: '/:notFound(.*)', component: NotFound }, // adding reg expression(.*) to allow the route to accept any input passed
     ], 
 });
